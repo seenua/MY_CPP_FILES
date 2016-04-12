bc03257f34f35a6b8c5e8f300ad8d060c6052964
seenua <seenuanandhan@gmail.com>
2016-04-12 14:59:19 +0530

    constructor-excises

---
+++ const.cpp
@@ -0,0 +1,34 @@
+// constructors and derived classes
+#include <iostream>
+using namespace std;
+
+class Mother {
+  public:
+    Mother ()
+      { cout << "Mother: no parameters\n"; }
+    Mother (int a)
+      { cout << "Mother: int parameter\n"; }
+};
+
+class Daughter : public Mother {
+  public:
+    Daughter (int a)
+      { cout << "Daughter: int parameter\n\n"; }
+};
+
+class Son : public Mother {
+  public:
+    Son (int a) : Mother (a)
+      { cout << "Son: int parameter\n\n"; }
+};
+
+int main () {
+  Daughter kelly(0);
+  Son bud(0);
+  
+  return 0;
+}
+
+
+//Daughter (int a)          // nothing specified: call default constructor
+//Son (int a) : Mother (a)  // constructor specified: call this specific constructor
